5cbd1c3aebcfbe1d46d2fd15814122ef51641d84
meti <49797027+metig@users.noreply.github.com>
2021-03-29 21:38:15 -0700

    work update

--- form/login/script.js
+++ form/login/script.js
@@ -1,1 +1,18 @@
 
+      // using addeventlistener
+ document.getElementById("submit").addEventListener("click", printCredential);
+
+     // using .onclick
+//   const submit = document.getElementById("submit");
+//   submit.onclick = printCredential;
+ 
+ function printCredential(){
+     // alternative
+     // var email = document.getElementById("email");
+
+     var email = document.form1.email.value;
+     var password = document.form1.password.value;
+     var website = document.form1.website.value;
+     console.log(email + "\n" + password + "\n" + website);
+    
+ }
--- jsPractice/printfibo.js
+++ jsPractice/printfibo.js
@@ -1,19 +1,17 @@
 function Fibo(n, a, b){
-   
-   if(a == 1){
-       return 0;
-   }
-   if(a == 2){
-       return 1;
-   }
-    //return Fibo(n-1) + Fibo(n-2);
-    return Fibo(a) + Fibo(b);
-  
-    
-}
-function printFibo(n,a,b){
-    for(let i =0; i<n; i++){
-        console.log(Fibo(i,a,b));
+
+    console.log(a);
+    console.log(b);
+
+    for(let i = 0; i <= n-2; i++){
+        let c = a + b;
+        console.log(c);
+        a = b;
+        b = c;
     }
+   
 }
-console.log(printFibo(9, 1 , 2));
+a = 0;
+b = 1;
+n = 7;
+console.log(Fibo(n, a, b))
